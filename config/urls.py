5b948cb2231d89e188bbf7bb1ee61576621109a9
Prakash Machhiwal <prakashmachhiwaltms@gmail.com>
2026-09-25 10:43:43 +0530

    Fix password visibility toggle listener clash and remove favicon

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
+from django.http import HttpResponse
+
 urlpatterns = [
+    path('favicon.ico', lambda request: HttpResponse(status=204)),
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('accounts/', include('accounts.urls')),
@@ -13,3 +16,4 @@ urlpatterns = [
     path('settings/', include('settings_app.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
+
